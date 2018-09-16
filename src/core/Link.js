58106acbeca33be0d6ff807528f5112a8452aed9
titaniumbaryum <titouan.baillon@gmail.com>
2018-09-16 19:41:13 +0200

    Link.js comments

--- src/core/Link.js
+++ src/core/Link.js
@@ -1,19 +1,20 @@
 class Link{
   constructor(i,n,o){
-    this.id=Math.round(Math.random()*1000000);
-    this.i=i;
-    this.o=o;
-    this.n=n;
-    this.p=false;
-    this.s=false;
-    this.d=this.i.connect(n,this);
+    this.id=Math.round(Math.random()*1000000);//unique identifier
+    this.i=i;//start node
+    this.o=o;//end node
+    this.n=n;//start node output name
+    this.p=false;//pause
+    this.s=false;//store activation if paused
+    this.sp={};//store the parameters of the last stored activation
+    this.d=this.i.connect(n,this);//start node output connection token
   }
   trigger(params){
     params.linkId=this.id;
-    if(!this.p)this.o.trigger(params);
-    else{
-      s=true;
-      sp=params;
+    if(!this.p)this.o.trigger(params);//trigger end node
+    else{//store the activation to be released at unpause
+      this.s=true;
+      this.sp=params;
     }
   }
   disconnect(){
@@ -28,9 +29,8 @@ class Link{
   unpause(){
     if(this.p && this.s){
       this.s=false;
-      this.p=false;
       this.trigger(this.sp);
     }
-    else this.p=false;
+    this.p=false;
   }
 }
