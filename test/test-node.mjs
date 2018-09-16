3a73339efd1007116411a7ca019c5e36b99cc810
titaniumbaryum <titouan.baillon@gmail.com>
2018-09-16 20:48:15 +0200

    working node test code

--- test/test-node.mjs
+++ test/test-node.mjs
@@ -5,4 +5,10 @@ import {ClockNode} from "../src/modules/ClockNode.mjs";
 import {DephaserNode} from "../src/modules/DephaserNode.mjs";
 import {SyncerNode} from "../src/modules/SyncerNode.mjs";
 var n={node:Node,clock:ClockNode,dephaser:DephaserNode,syncer:SyncerNode};
-let d = readFileSync("./test-descriptor.json", 'utf8');
+let d = JSON.parse(readFileSync("./test-descriptor.json", 'utf8'));
+if(!"nodes" in d || !"links" in d) throw "invalid data structure";
+for(let n of d.nodes){
+  if(typeof n.code == "string") n.code = eval(n.code);
+}
+var mesh =new MeshScript(d,n);
+setTimeout(function(){mesh.destroy();},10000);
